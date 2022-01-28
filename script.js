ee8dd32e8b88c03426307425c22bee116c08412e
jaybalais <archieabalais@gmail.com>
2022-01-27 18:08:22 -0800

    added generateTarget and compareGuesses function

--- script.js
+++ script.js
@@ -4,7 +4,22 @@ let currentRoundNumber = 1;
 
 // Write your code below:
 
+// This function will return a random integer between 0 and 9
 const generateTarget = () => {
     return Math.floor(Math.random() * 10)
 };
-//console.log(generateTarget());
+// console.log(generateTarget());
+
+// this function determines which guess is closest to the target number
+const compareGuesses = (humanNum, computerNum, targetNum) => {
+    let humanGuess = Math.abs(humanNum - targetNum);
+    let computerGuess = Math.abs(computerNum - targetNum);
+
+    if (humanGuess < computerGuess) {
+        return true;
+    } else {
+        return false;
+    }
+};
+
+// console.log(compareGuesses(3, 9, 2));
